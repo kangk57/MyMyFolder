27a96bc665c3881166f6de0d1bee09a57b2bafd5
kangk57 <kangk5712@gmail.com>
2021-11-04 17:26:05 +0900

    LEC11 실습
    가만히 있으면 눕고
    움직일땐 움직임

--- Activity/Lecture11_Character_Controller/boy.py
+++ Activity/Lecture11_Character_Controller/boy.py
@@ -1,14 +1,8 @@
+import grass
 from pico2d import *
 
 # Boy Event
-# fill here
-RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP = range(4)
-key_event_table = {
-    (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
-    (SDL_KEYDOWN, SDLK_LEFT): LEFT_DOWN,
-    (SDL_KEYUP, SDLK_RIGHT): RIGHT_UP,
-    (SDL_KEYUP, SDLK_LEFT): LEFT_UP
-}
+RIGHT_DOWN, LEFT_DOWN, RIGHT_UP, LEFT_UP, SLEEP_TIMER = range(5)
 
 key_event_table = {
     (SDL_KEYDOWN, SDLK_RIGHT): RIGHT_DOWN,
@@ -19,7 +13,6 @@ key_event_table = {
 
 
 # Boy States
-# fill here
 class IdleState:
     def enter(boy, event):
         if event == RIGHT_DOWN:
@@ -38,11 +31,15 @@ class IdleState:
     def do(boy):
         boy.frame = (boy.frame + 1) % 8
         boy.timer -= 1
+        if boy.timer == 0:
+            boy.add_event(SLEEP_TIMER)
+
     def draw(boy):
         if boy.dir == 1:
             boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
         else:
             boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
+
 
 class RunState:
     def enter(boy, event):
@@ -72,18 +69,35 @@ class RunState:
             boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
 
 
+class SleepState:
+
+    def enter(boy, event):
+        boy.frame = 0
+
+    def exit(boy, event):
+        pass
+
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+    def draw(boy):
+        if boy.dir == 1:
+            boy.image.clip_composite_draw(boy.frame * 100, 300, 100, 100, 3.141592 / 2,
+                                          '', boy.x - 25, boy.y - 25, 100, 100)
+        else:
+            boy.image.clip_composite_draw(boy.frame * 100, 200, 100, 100, -3.141592 / 2,
+                                          '', boy.x + 25, boy.y - 25, 100, 100)
+
+
 next_state_table = {
-# fill here
     IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState,
-                RIGHT_DOWN: RunState, LEFT_DOWN: RunState},
+                RIGHT_DOWN: RunState, LEFT_DOWN: RunState,
+                SLEEP_TIMER: SleepState},
     RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState,
-               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState}
+               LEFT_DOWN: IdleState, RIGHT_DOWN: IdleState},
+    SleepState: {LEFT_DOWN: RunState, RIGHT_DOWN: RunState,
+                 LEFT_UP: RunState, RIGHT_UP: RunState}
 }
-
-
-
-
-
 
 
 class Boy:
@@ -93,41 +107,52 @@ class Boy:
         self.image = load_image('animation_sheet.png')
         self.dir = 1
         self.velocity = 0
-        # fill here
         self.frame = 0
         self.timer = 0
         self.event_que = []
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
 
-
     def change_state(self,  state):
-        # fill here
         pass
 
-
     def add_event(self, event):
-        # fill here
         self.event_que.insert(0, event)
 
-
     def update(self):
-        # fill here
         self.cur_state.do(self)
-        if len(self.event_que) > 0 :
+        if len(self.event_que) > 0:
             event = self.event_que.pop()
             self.cur_state.exit(self, event)
             self.cur_state = next_state_table[self.cur_state][event]
             self.cur_state.enter(self, event)
 
     def draw(self):
-        # fill here
         self.cur_state.draw(self)
 
-
     def handle_event(self, event):
-        # fill here
         if (event.type, event.key) in key_event_table:
             key_event = key_event_table[(event.type, event.key)]
             self.add_event(key_event)
 
+    def draw(self):
+        self.cur_state.draw(self)
+        debug_print('Velocity :' + str(self.velocity) + '  Dir:' + str(self.dir))
+
+
+class Grass:
+    def __init__(self):
+        self.image = load_image('grass.png')
+
+    def draw(self):
+        self.image.draw(400, 30)
+
+
+def handle_events():
+    global running
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
